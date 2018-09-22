685a6cd9e13394af496d16ac41e6fc954fc95053
jgiallo <jfgiall2@ncsu.edu>
2018-09-22 18:19:24 -0400

    -Login class can now save users to the data base holllllla

--- db/migrate/20180922203608_create_house_hunters.rb
+++ db/migrate/20180922203608_create_house_hunters.rb
@@ -1,11 +1,6 @@
 class CreateHouseHunters < ActiveRecord::Migration[5.2]
   def change
     create_table :house_hunters do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-      t.string :phone
-      t.string :preferred_contact
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_22_201819) do
+ActiveRecord::Schema.define(version: 2018_09_22_212307) do
 
   create_table "house_hunters", force: :cascade do |t|
-    t.string "name"
-    t.string "email", default: "", null: false
-    t.string "password"
-    t.string "phone"
-    t.string "preferred_contact"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_house_hunters_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_house_hunters_on_reset_password_token", unique: true
   end
 
   create_table "houses", force: :cascade do |t|
@@ -74,6 +63,8 @@ ActiveRecord::Schema.define(version: 2018_09_22_201819) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "user_type"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
