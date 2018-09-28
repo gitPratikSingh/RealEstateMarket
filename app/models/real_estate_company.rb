3e48a696ca961ec3a34fa86901cdb8254b02b4c5
Pratik Singh <psingh22@ncsu.edu>
2018-09-28 01:05:04 -0400

    Added validations to admin model

--- app/models/real_estate_company.rb
+++ app/models/real_estate_company.rb
@@ -1,3 +1,14 @@
+require 'uri'
+
 class RealEstateCompany < ApplicationRecord
   has_many :realtors
+
+  validates :name, presence: true
+  validates :website, format: { with: URI::regexp(%w(http https)), :message => "can't be invalid URL"}
+  validates :address, presence: true
+  validates :size, presence: true
+  validates :founded, presence: true, numericality: true
+  validates :revenue, presence: true, numericality: true
+  validates :synopsis, presence: true
+
 end
