88eb6bb652d25da6c5dde1cf4abf1de666fc3222
Pratik Singh <psingh22@ncsu.edu>
2018-10-01 21:19:41 -0400

    signout

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  devise_for :users do
-    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
-  end
-
   get 'users/reset_user_type' => 'users#reset_user_type', as: 'reset_user_type'
   get 'house_hunters/set_user_type/:id' => 'house_hunters#set_user_type', as: 'set_type_house_hunter'
   get 'realtors/set_user_type/:id' => 'realtors#set_user_type', as: 'set_type_realtor'
